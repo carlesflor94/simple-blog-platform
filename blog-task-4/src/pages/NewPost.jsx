1f969b79820e96c0fbc97ff5c4adcbb4aab922f2
carlesflor94 <cf.ferrandis@gmail.com>
2026-02-08 05:53:42 +0100

    newpost html

--- blog-task-4/src/pages/NewPost.jsx
+++ blog-task-4/src/pages/NewPost.jsx
@@ -1,5 +1,22 @@
 import React from "react";
 
 export default function NewPost() {
-  return <div></div>;
+  return (
+    <div className="newpost-container">
+      <form>
+        <input type="text" placeholder="Title" />
+        <input type="text" placeholder="Short description" />
+        <input type="text" placeholder="Input your text" />
+
+        <div className="newpost-tags">
+          <button className="tags-button">one</button>
+          <button className="tags-button">something</button>
+          <button className="tags-button">chinese</button>
+          <button className="tags-button">english</button>
+          <button className="tags-button">spanish</button>
+        </div>
+        <button>Publish Article</button>
+      </form>
+    </div>
+  );
 }
