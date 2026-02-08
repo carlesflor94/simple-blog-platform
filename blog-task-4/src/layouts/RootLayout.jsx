9d7019218cc70cd15ae5230bf7859a33b9f55013
carlesflor94 <cf.ferrandis@gmail.com>
2026-02-08 07:39:13 +0100

    root layout html

--- blog-task-4/src/layouts/RootLayout.jsx
+++ blog-task-4/src/layouts/RootLayout.jsx
@@ -3,14 +3,26 @@ import { NavLink, Outlet } from "react-router-dom";
 export default function RootLayout() {
   return (
     <div className="root-layout">
-      <header>
-        <nav>
-          <h1>Realworld Blog</h1>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="newpost">New Post</NavLink>
-          <NavLink to="settings">Settings</NavLink>
-          <NavLink to="signin">Sign In</NavLink>
-        </nav>
+      <header className="app-header">
+        <div className="header-wrapper">
+          <div className="header-title">
+            <h1>Realworld Blog</h1>
+          </div>
+          <nav className="header-nav">
+            <NavLink to="/" className="header-nav-item">
+              Home
+            </NavLink>
+            <NavLink to="newpost" className="header-nav-item">
+              New Post
+            </NavLink>
+            <NavLink to="settings" className="header-nav-item">
+              Settings
+            </NavLink>
+            <NavLink to="signin" className="header-nav-item">
+              Sign In
+            </NavLink>
+          </nav>
+        </div>
       </header>
 
       <main>
