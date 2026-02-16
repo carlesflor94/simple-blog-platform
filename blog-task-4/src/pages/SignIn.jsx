2cb61ad1f02ff1eaba7b4225afbd221c9977f92e
carlesflor94 <cf.ferrandis@gmail.com>
2026-02-16 07:56:56 +0100

    login and signup function

--- blog-task-4/src/pages/SignIn.jsx
+++ blog-task-4/src/pages/SignIn.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import api from "../services/api";
 
 export default function SignIn() {
   const [formSignIn, setformSignIn] = useState({
@@ -7,20 +9,44 @@ export default function SignIn() {
     password: "",
   });
 
-  const handleSubmit = (e) => {
+  const [errors, setErrors] = useState(null);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setformSignIn({ ...formSignIn, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(formSignIn);
+
+    try {
+      const res = await api.post("/users/login", {
+        user: form,
+      });
+
+      login(res.data.user);
+      navigate("/");
+    } catch (err) {
+      setErrors(err.response?.data?.errors);
+    }
   };
 
   return (
     <div className="signin-container general-container">
       <h1 className="signin-title">Sign In</h1>
       <form onSubmit={handleSubmit} className="signin-form general-form">
+        {errors &&
+          Object.entries(errors).map(([key, value]) => (
+            <p key={key}>
+              {key} {value.join(", ")}
+            </p>
+          ))}
         <input
           type="text"
           className="general-user-input"
           placeholder="Username"
           value={formSignIn.username}
+          onChange={handleChange}
           required
         />
         <input
@@ -28,6 +54,7 @@ export default function SignIn() {
           className="general-user-input"
           placeholder="Password"
           value={formSignIn.password}
+          onChange={handleChange}
           required
         />
 
--- blog-task-4/src/pages/SignUp.jsx
+++ blog-task-4/src/pages/SignUp.jsx
@@ -1,30 +1,76 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../services/api";
+
 export default function SignUp() {
+  const [formSignUp, setFormSignUp] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState(null);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormSignUp({ ...formSignUp, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await api.post("/users", {
+        user: form,
+      });
+
+      login(res.data.user);
+      navigate("/");
+    } catch (err) {
+      setErrors(err.response?.data?.errors);
+    }
+  };
+
   return (
     <div className="signup-container general-container">
       <h1 className="signup-title">Sign Up</h1>
-      <form className="signup-form general-form">
+      <form onSubmit={handleSubmit} className="signup-form general-form">
+        {errors &&
+          Object.entries(errors).map(([key, value]) => (
+            <p key={key}>
+              {key} {value.join(", ")}
+            </p>
+          ))}
         <input
           type="text"
           className="general-user-input"
           placeholder="Username"
+          value={formSignUp.username}
+          onChange={handleChange}
           required
         />
         <input
           type="text"
           className="general-user-input"
           placeholder="Email address"
+          value={formSignUp.email}
+          onChange={handleChange}
           required
         />
         <input
           type="password"
           className="general-user-input"
           placeholder="Password"
+          value={formSignUp.password}
+          onChange={handleChange}
           required
         />
         <input
           type="password"
           className="general-user-input"
           placeholder="Repeat Password"
+          value={formSignUp.password}
+          onChange={handleChange}
           required
         />
 
