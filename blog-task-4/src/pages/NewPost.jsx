6cd02d7029d0e5f850fe8e38af6f061e753eb9e3
carlesflor94 <cf.ferrandis@gmail.com>
2026-02-18 13:37:41 +0100

    react hook form for NewPost page

--- blog-task-4/src/pages/NewPost.jsx
+++ blog-task-4/src/pages/NewPost.jsx
@@ -1,35 +1,35 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import api from "../services/api";
 import { useAuth } from "../context/AuthContext";
 import Button from "../components/Button";
+import { useForm } from "react-hook-form";
 
 export default function NewPost() {
   const navigate = useNavigate();
-  const [errors, setErrors] = useState(null);
-  const [formCreateArticle, setFormCreateArticle] = useState({
-    title: "",
-    description: "",
-    body: "",
-    tagList: [],
-  });
   const { user } = useAuth();
-
   const { slug } = useParams();
   const isEditing = !!slug;
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      title: "",
+      description: "",
+      body: "",
+      tagList: [],
+    },
+  });
 
-    setFormCreateArticle((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
+  const selectedTags = watch("tagList");
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
+  const onSubmit = async (formCreateArticle) => {
     try {
       let data;
       if (isEditing) {
@@ -42,29 +42,20 @@ export default function NewPost() {
         });
       }
       navigate(`/articles/${data.article.slug}`);
-    } catch (err) {
-      setErrors(
-        err.response?.data?.errors || {
-          general: ["Something went wrong."],
-        },
-      );
+    } catch {
+      alert("Article not created");
     }
   };
 
   const handleTagClick = (tag) => {
-    setFormCreateArticle((prev) => {
-      if (prev.tagList.includes(tag)) {
-        return {
-          ...prev,
-          tagList: prev.tagList.filter((t) => t !== tag),
-        };
-      } else {
-        return {
-          ...prev,
-          tagList: [...prev.tagList, tag],
-        };
-      }
-    });
+    if (selectedTags.includes(tag)) {
+      setValue(
+        "tagList",
+        selectedTags.filter((t) => t !== tag),
+      );
+    } else {
+      setValue("tagList", [...selectedTags, tag]);
+    }
   };
 
   /*editing view*/
@@ -72,7 +63,7 @@ export default function NewPost() {
   useEffect(() => {
     if (isEditing) {
       api.get(`/articles/${slug}`).then((data) => {
-        setFormCreateArticle({
+        reset({
           title: data.article.title,
           description: data.article.description,
           body: data.article.body,
@@ -80,38 +71,35 @@ export default function NewPost() {
         });
       });
     }
-  }, [slug, isEditing, user, navigate]);
+  }, [slug, isEditing, reset]);
 
   return (
     <div className="newpost-container general-container">
-      <form onSubmit={handleSubmit} className="newpost-form general-form">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="newpost-form general-form"
+      >
         <input
           type="text"
           className="general-user-input"
           placeholder="Title"
-          required
-          name="title"
-          value={formCreateArticle.title}
-          onChange={handleChange}
+          {...register("title", { required: "Title is required" })}
         />
+        {errors.title && <p>{errors.title.message}</p>}
         <input
           type="text"
           className="general-user-input"
           placeholder="Short description"
-          required
-          name="description"
-          value={formCreateArticle.description}
-          onChange={handleChange}
+          {...register("description", { required: "Description is required" })}
         />
+        {errors.description && <p>{errors.description.message}</p>}
         <textarea
           type="text"
           className="newpost-user-input-text"
           placeholder="Input your text"
-          required
-          name="body"
-          value={formCreateArticle.body}
-          onChange={handleChange}
+          {...register("body", { required: "Body is required" })}
         />
+        {errors.body && <p>{errors.body.message}</p>}
 
         <div className="general-tags">
           {["one", "something", "chinese", "english", "spanish"].map((tag) => (
@@ -120,7 +108,7 @@ export default function NewPost() {
               key={tag}
               onClick={() => handleTagClick(tag)}
               className={
-                formCreateArticle.tagList.includes(tag)
+                selectedTags.includes(tag)
                   ? "newpost-tag-selected"
                   : "newpost-tag-unselected"
               }
