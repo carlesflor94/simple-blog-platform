236c64a2d2a18de087b7f0b880f0704d3ef69f7c
carlesflor94 <cf.ferrandis@gmail.com>
2026-02-18 14:56:29 +0100

    react hook form for signin page

--- blog-task-4/src/pages/SignIn.jsx
+++ blog-task-4/src/pages/SignIn.jsx
@@ -1,27 +1,25 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import api from "../services/api";
 import { useAuth } from "../context/AuthContext";
 import Button from "../components/Button";
+import { useForm } from "react-hook-form";
 
 export default function SignIn() {
-  const [formSignIn, setformSignIn] = useState({
-    email: "",
-    password: "",
-  });
-
-  const [errors, setErrors] = useState(null);
   const navigate = useNavigate();
   const { login } = useAuth();
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-  const handleChange = (e) => {
-    setformSignIn({ ...formSignIn, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
+  const onSubmit = async (formSignIn) => {
     try {
       const data = await api.post("/users/login", {
         user: formSignIn,
@@ -31,9 +29,19 @@ export default function SignIn() {
       navigate("/");
     } catch (err) {
       if (err.response?.data?.errors) {
-        setErrors(err.response.data.errors);
+        Object.entries(err.response.data.errors).forEach(
+          ([field, messages]) => {
+            setError(field, {
+              type: "server",
+              message: messages.join(", "),
+            });
+          },
+        );
       } else {
-        setErrors({ general: ["Login failed. Please try again."] });
+        setError("root", {
+          type: "server",
+          message: "Login failed. Please try again.",
+        });
       }
     }
   };
@@ -41,31 +49,43 @@ export default function SignIn() {
   return (
     <div className="signin-container general-container">
       <h1 className="signin-title">Sign In</h1>
-      <form onSubmit={handleSubmit} className="signin-form general-form">
-        {errors &&
-          Object.entries(errors).map(([key, value]) => (
-            <p key={key}>
-              {key} {value.join(", ")}
-            </p>
-          ))}
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="signin-form general-form"
+      >
+        {errors.root && <p className="signup-error">{errors.root.message}</p>}
         <input
-          type="text"
+          type="email"
           className="general-user-input"
           placeholder="Email"
-          name="email"
-          value={formSignIn.email}
-          onChange={handleChange}
-          required
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^\S+@\S+$/i,
+              message: "Invalid email",
+            },
+          })}
         />
+        {errors.email && <p className="signup-error">{errors.email.message}</p>}
         <input
           type="password"
           className="general-user-input"
           placeholder="Password"
-          name="password"
-          value={formSignIn.password}
-          onChange={handleChange}
-          required
+          {...register("password", {
+            required: "Email is required",
+            minLength: {
+              value: 6,
+              message: "Password must be at least 6 characters",
+            },
+            maxLength: {
+              value: 40,
+              message: "Password must not exceed 40 characters",
+            },
+          })}
         />
+        {errors.password && (
+          <p className="signup-error">{errors.password.message}</p>
+        )}
 
         <div className="signin-bottom-container general-container">
           <p className="signin-text">
