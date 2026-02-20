be4b24a6e7876f811a7b247d9573c853613e81f5
carlesflor94 <cf.ferrandis@gmail.com>
2026-02-20 11:15:40 +0100

    fixed showing empty tags

--- blog-task-4/src/pages/Home.jsx
+++ blog-task-4/src/pages/Home.jsx
@@ -46,53 +46,65 @@ export default function Home() {
         </div>
 
         <div className="home-articles">
-          {articles.map((article) => (
-            <article
-              key={article.slug}
-              className="home-article-container general-container"
-            >
-              <div className="home-preview-header">
-                <div className="article-author">
-                  <div className="article-author-img">
-                    <img
-                      src={article.author.image || userImg}
-                      alt="user profile picture"
-                    />
+          {articles.map((article) => {
+            const validTags = (article.tagList || [])
+              .filter((tag) => typeof tag === "string")
+              .map((tag) => tag.trim())
+              .filter(Boolean);
+
+            return (
+              <article
+                key={article.slug}
+                className="home-article-container general-container"
+              >
+                <div className="home-preview-header">
+                  <div className="article-author">
+                    <div className="article-author-img">
+                      <img
+                        src={article.author.image || userImg}
+                        alt="user profile picture"
+                      />
+                    </div>
+                    <div className="article-author-details">
+                      <p className="article-author-name">
+                        <Link
+                          to={`/profile/${article.author.username}`}
+                          className="article-author-link"
+                        >
+                          {article.author.username}
+                        </Link>
+                      </p>
+                      <p className="article-date">
+                        {dateFormat(article.createdAt)}
+                      </p>
+                    </div>
                   </div>
-                  <div className="article-author-details">
-                    <p className="article-author-name">
-                      <Link
-                        to={`/profile/${article.author.username}`}
-                        className="article-author-link"
-                      >
-                        {article.author.username}
-                      </Link>
-                    </p>
-                    <p className="article-date">
-                      {dateFormat(article.createdAt)}
-                    </p>
+                  <div className="preview-likes-container">
+                    <div className="preview-likes-heart">♥</div>
+                    <div className="preview-likes-counter">
+                      {article.favoritesCount}
+                    </div>
                   </div>
                 </div>
-                <div className="preview-likes-container">
-                  <div className="preview-likes-heart">♥</div>
-                  <div className="preview-likes-counter">
-                    {article.favoritesCount}
+                <h2 className="home-article-title">
+                  <Link
+                    to={`/articles/${article.slug}`}
+                    className="article-link"
+                  >
+                    {article.title}
+                  </Link>
+                </h2>
+                <p className="home-article-text">{article.description}</p>
+                {validTags?.length > 0 && (
+                  <div className="general-tags">
+                    {validTags.map((tag) => (
+                      <button key={tag}>{tag}</button>
+                    ))}
                   </div>
-                </div>
-              </div>
-              <h2 className="home-article-title">
-                <Link to={`/articles/${article.slug}`} className="article-link">
-                  {article.title}
-                </Link>
-              </h2>
-              <p className="home-article-text">{article.description}</p>
-              <div className="general-tags">
-                {article.tagList.map((tag) => (
-                  <button key={tag}>{tag}</button>
-                ))}
-              </div>
-            </article>
-          ))}
+                )}
+              </article>
+            );
+          })}
         </div>
 
         <Pagination
